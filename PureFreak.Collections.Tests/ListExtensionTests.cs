fa2b95995026ae25fe5eb6b3b0196d29af3de6f4
Kai <mail@kaisnet.de>
2018-08-24 21:37:23 +0200

    Added ListExtensions

---
+++ PureFreak.Collections.Tests/ListExtensionTests.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace PureFreak.Collections.Tests
+{
+    [TestClass]
+    public class ListExtensionTests
+    {
+        [TestMethod]
+        public void SwitchTest()
+        {
+            var list = new List<int>();
+            list.Add(10);
+            list.Add(20);
+
+            list.Switch(0, 1);
+
+            Assert.AreEqual(20, list[0]);
+            Assert.AreEqual(10, list[1]);
+        }
+
+        [TestMethod]
+        public void InteractTest()
+        {
+            var list = new List<int>();
+            for (int i = 1; i <= 3; i++)
+                list.Add(i);
+
+            var callbackCallCounter = 0;
+            list.Interact((f, s) =>
+            {
+                callbackCallCounter++;
+                switch (callbackCallCounter)
+                {
+                    case 1:
+                        Assert.AreEqual(f, 1);
+                        Assert.AreEqual(s, 2);
+                        break;
+                    case 2:
+                        Assert.AreEqual(f, 1);
+                        Assert.AreEqual(s, 3);
+                        break;
+
+                    case 3:
+                        Assert.AreEqual(f, 2);
+                        Assert.AreEqual(s, 1);
+                        break;
+                    case 4:
+                        Assert.AreEqual(f, 2);
+                        Assert.AreEqual(s, 3);
+                        break;
+
+                    case 5:
+                        Assert.AreEqual(f, 3);
+                        Assert.AreEqual(s, 1);
+                        break;
+                    case 6:
+                        Assert.AreEqual(f, 3);
+                        Assert.AreEqual(s, 2);
+                        break;
+                }
+            });
+
+            Assert.AreEqual(6, callbackCallCounter);
+        }
+
+        [TestMethod]
+        public void MyTestMethod()
+        {
+            var list = new List<int>();
+            for (int i = 1; i <= 10; i++)
+                list.Add(i);
+
+            var listClone = new List<int>(list);
+            listClone.Shuffle(new Random());
+
+            CollectionAssert.AreNotEqual(listClone, list);
+        }
+    }
+}
---
+++ PureFreak.Collections/Extensions/ListExtensions.cs
@@ -0,0 +1,62 @@
+﻿namespace System.Collections.Generic
+{
+    public static class ListExtensions
+    {
+        #region Methods
+
+        public static void Switch<T>(this List<T> list, int index1, int index2)
+        {
+            if (list == null)
+                throw new ArgumentNullException(nameof(list));
+
+            if (index1 < 0 || index1 >= list.Count)
+                throw new ArgumentOutOfRangeException(nameof(index1));
+            if (index2 < 0 || index2 >= list.Count)
+                throw new ArgumentOutOfRangeException(nameof(index2));
+
+            if (index1 == index2)
+                return;
+
+            var tmp = list[index1];
+
+            list[index1] = list[index2];
+            list[index2] = tmp;
+        }
+
+        public static void Interact<T>(this List<T> list, Action<T, T> callback)
+        {
+            if (list == null)
+                throw new ArgumentNullException(nameof(list));
+            if (callback == null)
+                throw new ArgumentNullException(nameof(callback));
+
+            for (int index = 0; index < list.Count; index++)
+            {
+                for (int subIndex = 0; subIndex < list.Count; subIndex++)
+                {
+                    if (index != subIndex)
+                    {
+                        callback(list[index], list[subIndex]);
+                    }
+                }
+            }
+        }
+
+        public static void Shuffle<T>(this List<T> list, Random random)
+        {
+            var max = list.Count;
+            while (max > 1)
+            {
+                max--;
+
+                var rndIndex = random.Next(max + 1);
+                var rndValue = list[rndIndex];
+
+                list[rndIndex] = list[max];
+                list[max] = rndValue;
+            }
+        }
+
+        #endregion
+    }
+}
