3d36bcce3f6b10ad4a1ed72c2e8e224c60d5efc5
Kai Gartenschlaeger <mail@kaisnet.de>
2018-08-28 11:21:10 +0200

    Added BagTests class

---
+++ PureFreak.Collections.Tests/BagTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PureFreak.Collections.Tests
+{
+    [TestClass]
+    public class BagTests
+    {
+        #region Test methods
+
+        [TestMethod]
+        public void AddTest()
+        {
+            var bag = new Bag<int>();
+
+            for (int i = 1; i <= 5; i++)
+                bag.Add(i);
+
+            Assert.AreEqual(5, bag.Count);
+
+            for (int i = 1; i <= 5; i++)
+                Assert.AreEqual(i, bag.Get(i - 1));
+        }
+
+        #endregion
+    }
+}
